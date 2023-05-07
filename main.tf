643eb76eb25bc55e1cc33a6efb871061caed0b69
eyarga <yargaelvis@gmail.com>
2023-05-07 15:46:41 -0400

    Update main.tf
    update name

--- main.tf
+++ main.tf
@@ -155,7 +155,7 @@ resource "aws_s3_bucket" "my-new-s3-bucket" {
 }
 
 resource "aws_security_group" "my-new-security-group" {
-  name        = "web_server_inbound"
+  name        = "Web_server_inbound"
   description = "Allow inbound traffic on tcp/443"
   vpc_id      = aws_vpc.vpc.id
 
